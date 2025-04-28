2e572e533d8b1876976670f39ae01d4397874871
mrosxam <m_rosxam@mit.edu>
2025-04-27 21:44:41 -0400

    DONE!

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
-
+app.use(express.static(path.join(__dirname, '.')));
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
