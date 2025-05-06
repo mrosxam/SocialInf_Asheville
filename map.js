ec1b9ed6feafbc97a68db68ac656a5888abde0ca
mrosxam <m_rosxam@mit.edu>
2025-05-05 22:16:33 -0400

    Update map.js

--- map.js
+++ map.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = "";
 // CREATE A NEW OBJECT CALLED MAP
 const map = new mapboxgl.Map({
 container: "map", // container ID for the map object (this points to the HTML element)
-style: "mapbox://styles/miquelxamena/cm9rxg6jx00vv01s53zcd93q8", //YOUR STYLE URL
+style: "", //YOUR STYLE URL
 center: [-82.548205, 35.601878], // starting position [lng, lat] 
 zoom: 11.18,
 projection: "globe", // display the map as a 3D globe
